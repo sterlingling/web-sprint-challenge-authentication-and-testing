648c48a8d953edc09dd365806ce64cc4c75de9ab
Sterling Sanders <sterlingsanders723@gmail.com>
2024-02-22 15:14:44 -0800

    update

--- api/auth/user-model.js
+++ api/auth/user-model.js
@@ -3,11 +3,20 @@ const db = require('../../data/dbConfig')
 function findById(user_id) {
     return db('users').select('user_id', 'username').where('user_id', user_id).first();
 }
-
 async function add(user) {
-    const [id] = await db('users').insert(user);
-    return findById(id);
-}
+    // Insert the user into the database
+    const [userId] = await db('users').insert(user);
+  
+    // Retrieve the newly inserted user from the database
+    const newUser = await db('users').where('id', userId).first();
+  
+    return newUser;
+  }
+
+// async function add(user) {
+//     const [id] = await db('users').insert(user);
+//     return findById(id);
+// }
 
 module.exports = {
     add,
