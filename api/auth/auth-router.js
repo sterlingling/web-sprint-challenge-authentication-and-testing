f62ff47961f5da38d907eba684d7ecc123e0df79
Sterling Sanders <sterlingsanders723@gmail.com>
2024-02-22 16:53:55 -0800

    done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { jwtSecret } = require('./config');
+const { JWT_SECRET } = require('./config');
 const User = require('./user-model')
 router.post('/register', async (req, res, next) => {
   const { username, password } = req.body;
@@ -23,41 +23,41 @@ router.post('/register', async (req, res, next) => {
 
     // Add the new user to the database
     const newUser = await User.add({ username, password: hash });
-    console.log(newUser);
+console.log(newUser);
     // Respond with the new user's information
     res.status(201).json(newUser);
   } catch (error) {
     next(error);
   }
 });
-/*
-  IMPLEMENT
-  You are welcome to build additional middlewares to help with the endpoint's functionality.
-  DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+  /*
+    IMPLEMENT
+    You are welcome to build additional middlewares to help with the endpoint's functionality.
+    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
 
-  1- In order to register a new account the client must provide `username` and `password`:
-    {
-      "username": "Captain Marvel", // must not exist already in the `users` table
-      "password": "foobar"          // needs to be hashed before it's saved
-    }
+    1- In order to register a new account the client must provide `username` and `password`:
+      {
+        "username": "Captain Marvel", // must not exist already in the `users` table
+        "password": "foobar"          // needs to be hashed before it's saved
+      }
 
-  2- On SUCCESSFUL registration,
-    the response body should have `id`, `username` and `password`:
-    {
-      "id": 1,
-      "username": "Captain Marvel",
-      "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-    }
+    2- On SUCCESSFUL registration,
+      the response body should have `id`, `username` and `password`:
+      {
+        "id": 1,
+        "username": "Captain Marvel",
+        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
+      }
 
-  3- On FAILED registration due to `username` or `password` missing from the request body,
-    the response body should include a string exactly as follows: "username and password required".
+    3- On FAILED registration due to `username` or `password` missing from the request body,
+      the response body should include a string exactly as follows: "username and password required".
 
-  4- On FAILED registration due to the `username` being taken,
-    the response body should include a string exactly as follows: "username taken".
-*/
+    4- On FAILED registration due to the `username` being taken,
+      the response body should include a string exactly as follows: "username taken".
+  */
 
 
-router.post('/login', async (req, res) => {
+router.post('/login', async(req, res) => {
   const { username, password } = req.body;
 
   try {
@@ -82,7 +82,7 @@ router.post('/login', async (req, res) => {
     const token = jwt.sign({
       userId: user.id,
       username: user.username,
-    }, jwtSecret, { expiresIn: '1h' });
+    }, JWT_SECRET, { expiresIn: '1h' });
 
     // Respond with success message and token
     res.status(200).json({
